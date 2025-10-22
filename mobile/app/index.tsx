438326f28c29c756c897cec061c105d7c0230628
ayan paul <ayanpaulcse08@gmail.com>
2025-10-22 16:13:39 +0530

    telwind attached

---
+++ mobile/app/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-xl font-semibold text-center text-black">
+        Welcome to Nativewind!
+      </Text>
+    </View>
+  );
+}
