4c26159652c9359c7b05c0ce81a02f1590ed7cbf
ayan paul <ayanpaulcse08@gmail.com>
2025-10-22 13:43:28 +0530

    small chnage

--- Backend/src/middleware/arcjet.middleware.js
+++ Backend/src/middleware/arcjet.middleware.js
@@ -10,17 +10,17 @@ export const arcjetMiddleware = async (req, res, next) => {
 
         if (decision.isDenied) {
             if (decision.reason.isRateLimit()) {
-                res.status(429).json({ message: "Too Many Requests. Please try again later." });
+                return res.status(429).json({ message: "Too Many Requests. Please try again later." });
             } else if (decision.reason.isBot()) {
-                res.status(403).json({ message: "Access Denied. Bot traffic is not allowed." });
+                return res.status(403).json({ message: "Access Denied. Bot traffic is not allowed." });
             } else {
-                res.status(403).json({ message: "Access Denied." });
+                return res.status(403).json({ message: "Access Denied." });
             }
         }
 
         //check for spoofed bots
         if (decision.results.some((result) => result.reason.isBot() && result.reason.isSpoofed())) {
-            res.status(403).json({
+            return res.status(403).json({
                 error: "Spoofed bot detected. Access denied.",
                 message: "Malicious bot activity detected"
             });
@@ -29,7 +29,7 @@ export const arcjetMiddleware = async (req, res, next) => {
         next();
     } catch (error) {
         console.error("Arcjet Middleware Error:", error);
-        res.status(500).json({ message: "Internal Server Error." });
+        return res.status(500).json({ message: "Internal Server Error." });
         next();
     }
 };
