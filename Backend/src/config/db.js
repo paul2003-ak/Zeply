6f757e250f4dcecab15a491cc097b94e9d5e91cf
paul2003-ak <bablijoy08@gmail.com>
2025-10-26 10:59:14 +0530

    Update MONGODB_URI log snippet length
    Increase the snippet length of MONGODB_URI in logs.

--- Backend/src/config/db.js
+++ Backend/src/config/db.js
@@ -5,7 +5,7 @@ export const connectDb = async () => {
   try {
     console.log("🟢 Connecting to MongoDB...");
     console.log("MONGODB_URI:", ENV.MONGODB_URI ? "Loaded" : "Missing");
-    console.log("🔍 MONGODB_URI value snippet:", ENV.MONGODB_URI?.slice(0, 30)); // start ka part only
+    console.log("🔍 MONGODB_URI value snippet:", ENV.MONGODB_URI?.slice(0, 50)); // start ka part only
 
     await mongoose.connect(ENV.MONGODB_URI, {
       serverSelectionTimeoutMS: 30000
