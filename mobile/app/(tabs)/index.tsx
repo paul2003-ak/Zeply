f138f6ca65b3dd1fa6af0190b9716f6da8f91614
ayan paul <ayanpaulcse08@gmail.com>
2025-10-26 13:25:13 +0530

    chnage something

--- mobile/app/(tabs)/index.tsx
+++ mobile/app/(tabs)/index.tsx
@@ -2,8 +2,10 @@ import { Text } from 'react-native'
 import React from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import SignOutButton from '@/components/signOutButton'
+import { useUserSync } from '@/hooks/useUsersync'
 
 const Home = () => {
+  useUserSync()
   return (
     <SafeAreaView>
         <Text>Home</Text>
