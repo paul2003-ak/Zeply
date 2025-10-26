4ca1e3151ccf87cfee4516efa1a7f8477bbbbc61
paul2003-ak <bablijoy08@gmail.com>
2025-10-26 10:47:39 +0530

    Update MongoDB connection settings
    Removed TLS option from mongoose connection settings.

--- Backend/src/config/db.js
+++ Backend/src/config/db.js
@@ -5,15 +5,14 @@ export const connectDb = async () => {
   try {
     console.log("🟢 Connecting to MongoDB...");
     console.log("MONGODB_URI:", ENV.MONGODB_URI ? "Loaded" : "Missing");
+    console.log("🔍 MONGODB_URI value snippet:", ENV.MONGODB_URI?.slice(0, 30)); // start ka part only
 
     await mongoose.connect(ENV.MONGODB_URI, {
-      serverSelectionTimeoutMS: 30000, // wait up to 30s
-      tls: true,
+      serverSelectionTimeoutMS: 30000
     });
 
     console.log("✅ Database connected successfully");
   } catch (error) {
     console.error("❌ Database connection failed:", error.message);
-    process.exit(1);
   }
 };
